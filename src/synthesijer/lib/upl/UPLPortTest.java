fff8fd67ccc40e5366edffd71619f3c3fca5268c
Takefumi MIYOSHI <miyo@wasamon.net>
2014-07-24 18:39:20 +0900

    made UPLPortTest simple.

--- src/synthesijer/lib/upl/UPLPortTest.java
+++ src/synthesijer/lib/upl/UPLPortTest.java
@@ -7,13 +7,14 @@ public class UPLPortTest {
 	public static int DATA_OFFSET = 4;
 	public static int DATA_LENGTH = 3;
 	
-	
-	private int ch;
-	
-	private void toLow(){
+	private int toLow(int ch){
+		int ret;
 		if(ch >= 'A' && ch <= 'Z'){
-			ch = ch - 'A' + 'a';
+			ret = ch - 'A' + 'a';
+		}else{
+			ret = ch;
 		}
+		return ret;
 	}
 	
 	public void run(){
@@ -25,18 +26,10 @@ public class UPLPortTest {
 		for(int i = 0; i < (len >> 2); i++){
 			int v  = port.data[i+DATA_OFFSET];
 			int r = 0;
-			ch = (v >> 0) & 0x000000FF;
-			toLow();
-			r += ch << 0;
-			ch = (v >> 8) & 0x000000FF;
-			toLow();
-			r += ch << 8;
-			ch = (v >> 16) & 0x000000FF;
-			toLow();
-			r += ch << 16;
-			ch = (v >> 24) & 0x000000FF;
-			toLow();
-			r += ch << 24;
+			r += (toLow((v >> 0) & 0x000000FF)) << 0;
+			r += (toLow((v >> 8) & 0x000000FF)) << 8;
+			r += (toLow((v >> 16) & 0x000000FF)) << 16;
+			r += (toLow((v >> 24) & 0x000000FF)) << 24;
 			port.data[i+DATA_OFFSET] = r;
 		}
 		
