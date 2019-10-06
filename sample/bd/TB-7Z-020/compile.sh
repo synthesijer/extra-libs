60add02c45e88f0e317716cb8ed940312687bb9a
Takefumi MIYOSHI <miyo@wasamon.net>
2019-10-06 20:40:12 +0900

    modified the default environment value for SYNTHESIJER in Makefiles

---
+++ sample/bd/TB-7Z-020/compile.sh
@@ -0,0 +1,7 @@
+java -cp ../../../bin:../../../../target/synthesijer:. \
+  synthesijer.Main \
+  ../../../extra-libs/src/synthesijer/lib/axi/SimpleAXIMemIface32RTL.java \
+  ../../../extra-libs/src/synthesijer/lib/axi/SimpleAXIMemIface32RTLTest.java \
+  TestFrame.java \
+  RGBTest.java \
+  SinTableRom.java
